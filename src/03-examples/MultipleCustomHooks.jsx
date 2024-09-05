0c63ddd7a94e3a765040515bc13bf4ba42bb6104
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-05 14:27:46 -0400

    Clase: Parametrizar y consumir nuestro custom hook
    Puliendo mas el custom hook useFetch y utilizando con las mejorar en el componente MultipleCustomHooks.jsx

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,16 @@
 import { useFetch } from "../hooks";
 
 export const MultipleCustomHooks = () => {
-    useFetch();
+    const {data, isLoading, hasError} = useFetch('https://pokeapi.co/api/v2/pokemon/3');
     return(
         <>
-            <h1> Custom hooks multiples</h1>
+            <h1> Info de pokemones </h1>
+            <hr />
+
+            { isLoading && <p>Cargando...</p> }
+
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+            <h2> {data?.name} </h2>
         </>
     );
 };
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = () => {
+export const useFetch = ( url ) => {
     const [ state, setState ]= useState({
         data: null,
         isLoading: true,
@@ -10,11 +10,44 @@ export const useFetch = () => {
 
     useEffect(() => {
         getFetch();
-    }, []);
+    }, [url]);
+
+    const setLoadingState = () => {
+        setState({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            error: null,
+        });
+    };
 
     const getFetch = async() => {
-        const response = await fetch('https://pokeapi.co/api/v2/');
+        setLoadingState();
+
+        const response = await fetch(url);
+
+        await new Promise( resolve => setTimeout( resolve, 1000 ) );
+
+        if ( !response.ok){
+            setState({
+                data:null,
+                isLoading: false,
+                hasError: true,
+                error: {
+                    code: response.status,
+                    message: response.statusText,
+                }
+            })
+            return;
+        }
+
         const data = await response.json();
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            error: null,
+        });
         console.log({data});
     };
 
