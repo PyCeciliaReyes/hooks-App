c67bec0baa66a1a9b9fc978f241a9568aef91077
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-06 14:22:58 -0400

    Clase: tarea de useTodo
    Creacion de un hook custom que contiene todos las comunicaciones y retornar como un objeto. Asi tambien reducir lineas de codigos en el componente principal TodoApp.jsx

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,61 +1,14 @@
-import { useEffect, useReducer } from 'react';
-import { todoReducer } from './todoReducer';
 import { TodoList } from './TodoList';
 import { TodoAdd } from './TodoAdd';
+import { useTodos } from '../hooks';
 
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'recolectar la piedra del alma',
-    //     done: false,
-    // },
-    // {
-    //     id: new Date().getTime() * 3,
-    //     description: 'recolectar la piedra del poder',
-    //     done: false,
-    // },
-]
-
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || [];
-};
 
 export const TodoApp = () => {
-
-    const [ todos, dispatch ]= useReducer(todoReducer, initialState, init);
-
-    useEffect(() =>{
-        localStorage.setItem('todos', JSON.stringify( todos ) );
-    }, [todos])
-
-    const handleNewTodo = (todo) => {
-        // console.log({todo})
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo
-        }
-        dispatch( action )
-    }
-
-    const handleDeleteTodo = (id) => {
-        //console.log(id)
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        });
-    };
-
-    const handleToggleTodo = (id) => {
-        //console.log(id)
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id
-        });
-    }
-
+    const {todos, handleNewTodo, handleDeleteTodo, handleToggleTodo, todosCount, pendingTodosCount} = useTodos()
+    
     return(
         <>
-            <h1>TodoApp: 10 - <small>Pendientes: 2</small> </h1>
+            <h1>TodoApp: {todosCount} - <small>Pendientes: { pendingTodosCount }  </small> </h1>
             <hr />
             <div className='row'>
                 <div className='col-7'>
