f8ac9900f3203c17f68fbd36ceccbd9a2a965008
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-06 09:41:36 -0400

    clase: Idea gnal de un reducer - via codigo.
    Ideas generales del reduce, agregando un nuevo to do al objeto a traves de la funcion todoReducer

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -1,1 +1,31 @@
-console.log('hola mundo');
+const initialState = [{
+    id: 1,
+    todo:'recoletar la piedra del alma',
+    done: false,
+}];
+
+const todoReducer = ( state = initialState, action = {} ) => {
+    
+    if( action.type === '[TODO], add todo'){
+        return [...state, action.payload];
+    }
+    return state;
+};
+
+let todos = todoReducer();
+
+const newTodo = {
+    id: 2,
+    todo:'recoletar la piedra del poder',
+    done: false,
+}
+
+const AddTodoAction = {
+    type: '[TODO], add todo',
+    payload: newTodo,
+};
+
+todos = todoReducer( todos, AddTodoAction );
+
+
+console.log({state: todos})
