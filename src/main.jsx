831451d46e9fd1e48379987e22346a0192ffe11e
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-05 21:26:53 -0400

    clase: continuacion del proyecto - HookApp
    preparando el ambiente ed trabajo

--- src/main.jsx
+++ src/main.jsx
@@ -13,11 +13,12 @@ import { createRoot } from 'react-dom/client';
 //import { MemoHook } from './06-memos/MemoHook';
 //import { CallBackHook } from './06-memos/CallBackHook';
 import { Padre } from './07-tarea-memo/Padre';
+import './08-useReducer/intro-reducer';
 
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Padre/>
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Padre/>
+//   </StrictMode>,
+// )
