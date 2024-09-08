cfac25f077ca933dafdf891bbf2fff77bcca8e59
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-08 11:46:02 -0400

    Clase: ejecutar funciones del customHook dentro de las pruebas
    haciendo pruebas en las funciones de increment, decrement y reset

--- test/hooks/useCounter.test.js
+++ test/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { render, renderHook } from "@testing-library/react";
+import { act, fireEvent, render, renderHook } from "@testing-library/react";
 import { useCounter } from "../../src/hooks/useCounter";
 
 describe('Pruebas en el useCounter.js', () => {
@@ -20,5 +20,33 @@ describe('Pruebas en el useCounter.js', () => {
         const {result} = renderHook( () => useCounter(initialValue) );
         const {counter} = result.current;
         expect(counter).toBe(100);
+    });
+
+    test('debe de incrementar el contador', () => {
+        const {result} = renderHook( () => useCounter(initialValue) );
+        const {increment} = result.current;
+        act( () => {
+            increment();
+        })
+        expect(result.current.counter).toBe(101);
+    });
+
+    test('debe de decrementar el contador', () => {
+        const {result} = renderHook( () => useCounter(initialValue) );
+        const {decrement} = result.current;
+        act( () => {
+            decrement();
+        })
+        expect(result.current.counter).toBe(99);
+    });
+
+    test('debe de hacer el reset del contador', () => {
+        const {result} = renderHook( () => useCounter(initialValue) );
+        const {reset,increment} = result.current;
+        act( () => {
+            increment();
+            reset();
+        })
+        expect(result.current.counter).toBe(100);
     });
 });
